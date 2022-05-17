c53405ab8676bc962030554f6f3bd4a04f3b1736
pdimitriu <pdimitri@spawar.navy.mil>
2022-05-17 08:19:49 -0700

    removing db mods

--- db/mods/mod_001.sql
+++
@@ -1,5 +0,0 @@
-alter table account add column "created" datetime NOT NULL;
-alter table account add column "modified" datetime NOT NULL;
-alter table account add column "account_id" varchar(15) NOT NULL default '0';
-alter table account add column "guid" varchar(15) NOT NULL default '0';
-alter table account drop column account_id;
