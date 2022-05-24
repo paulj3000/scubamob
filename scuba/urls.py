424d5fc2d25acd8e90c6da7076125ae593a63b37
pdimitriu <pdimitri@spawar.navy.mil>
2022-05-24 07:40:12 -0700

    deleting some dead files and directories introducing memcache for caching purposes

--- scuba/urls.py
+++ scuba/urls.py
@@ -44,7 +44,6 @@ urlpatterns = [
     path('api/1.0/diveshops/', include('api.urls_diveshops')),
 
     path('api/1.0/mobile/account', include('api.urls_account')),
-    path('api/1.0/mobile', include('api.urls_mobile')),
     path('api/accounts/', include('scuba.accounts.urls_accounts_api')),
 
 
