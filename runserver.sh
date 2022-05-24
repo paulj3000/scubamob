8279d9d7baf1f73c3250ec1fabf01202626052fa
pdimitriu <pdimitri@spawar.navy.mil>
2022-05-24 08:19:15 -0700

    forcing docker to run memcached and mongo at startup

--- runserver.sh
+++ runserver.sh
@@ -1,1 +1,9 @@
-python manage.py runserver --nothreading 0.0.0.0:8002
+if [[ -z $(docker ps | grep "mongo" | awk '{ print $1 }')  ]]; then
+docker run -it -d -p 27017:27017 mongo
+fi
+
+if [[ -z $(docker ps | grep "memcached" | awk '{ print $1 }')  ]]; then
+docker run -it -d -p 11211:11211 memcached
+fi
+
+python manage.py runserver 0.0.0.0:8002
