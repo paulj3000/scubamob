d054a3f8105beb3d0e212dcdaa0a11b3535cf48d
Pauljames Dimitriu <paulj1999@yahoo.com>
2023-03-24 11:25:26 -0700

    removing need for serializer in class

--- scuba/accounts/apis/password.py
+++ scuba/accounts/apis/password.py
@@ -2,15 +2,15 @@ import logging
 
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
 
 from django.contrib.auth.forms import PasswordResetForm
 
 logger = logging.getLogger(__name__)
 
 
-class PasswordResetApi(generics.GenericAPIView):
+class PasswordResetApi(APIView):
     permission_classes = (AllowAny,)
     def post(self, request):
         """ post
