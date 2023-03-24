ac2864b8c7122012e7978306722e36472f841c18
Pauljames Dimitriu <paulj1999@yahoo.com>
2023-03-24 11:15:33 -0700

    adding logger to password reset

--- scuba/accounts/apis/password.py
+++ scuba/accounts/apis/password.py
@@ -1,9 +1,13 @@
+import logging
+
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
 from django.contrib.auth.forms import PasswordResetForm
+
+logger = logging.getLogger(__name__)
 
 
 class PasswordResetApi(generics.GenericAPIView):
@@ -17,6 +21,9 @@ class PasswordResetApi(generics.GenericAPIView):
         #self.serializer_class(data=request.data)
 
         form = PasswordResetForm(request.data)
+
+        # send this off to our logger
+        logger = logging.info(f"Password reset for: {request.data['email']}")
         if form.is_valid():
             form.save()
             return Response(status=status.HTTP_200_OK)
