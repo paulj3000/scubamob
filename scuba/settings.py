4efcc890b7b7dbed8227a5f85b06d5f55e12ca7c
Pauljames Dimitriu <paulj1999@yahoo.com>
2023-03-24 10:00:59 -0700

    removing 'django_rest_passwordreset' by mistake

--- scuba/settings.py
+++ scuba/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django_rest_passwordreset',
     'django_extensions',
     'corsheaders',
     #'test_pep8',
