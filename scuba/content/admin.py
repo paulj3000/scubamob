0909e9daaa5a5fe521cfdf3c4b3afdf4136457ae
Pauljames Dimitriu <paulj1999@yahoo.com>
2023-03-26 23:21:11 -0700

    admin is done

--- scuba/content/admin.py
+++ scuba/content/admin.py
@@ -38,11 +38,16 @@ class EmailTemplateAdmin(admin.ModelAdmin):
         """
         email_template = get_object_or_404(EmailTemplate, pk=id)
 
+        to_send_email = {
+            2: user.send_confirmation_code_email
+        }
+
         for user in User.objects.filter(
                 Q(is_superuser=True) |
                 Q(is_admin=True)):
 
-            user.send_confirmation_code_email(email_template)
+            to_send_email[email_template.template_type](email_template)
+            #user.send_confirmation_code_email(email_template, '123456')
 
         messages.add_message(
             request,
