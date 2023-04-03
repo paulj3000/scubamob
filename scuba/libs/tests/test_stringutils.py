0966694928c58faed54f6938fbb2951f42403078
pdimitriu <pdimitri@spawar.navy.mil>
2023-04-03 07:02:18 -0700

    adding a test for code coverage purposes

--- scuba/libs/tests/test_stringutils.py
+++ scuba/libs/tests/test_stringutils.py
@@ -12,7 +12,8 @@ from scuba.libs.stringutils import StringUtils
 class TestStringUtils(TestCase):
     def test_generate_url_from_string(self):
         """
-        Test simple user get name
+        Testing the conversion of a simple string to a
+        url-safe string
         """
         # just for readability and pep8 stuff
         gen_url = StringUtils.generate_url_from_string
@@ -21,9 +22,22 @@ class TestStringUtils(TestCase):
         self.assertEqual(gen_url('Whites Point 987'), 'whites-point-987')
 
     def test_generate_random_string(self):
+        """
+        Testing the generation of a random string, case sensitive
+        """
         gen_str = StringUtils.generate_random_string
 
         for some_len in [10, 5, 3, 20]:
             some_str = gen_str(some_len)
             self.assertEqual(len(some_str), some_len)
             self.assertTrue(some_str.isupper())
+
+    def test_generate_random_string_case_insensitive(self):
+        """
+        Testing the generation of a random string, case insensitive
+        """
+        gen_str = StringUtils.generate_random_string_case_insensitive
+
+        for some_len in [10, 5, 3, 20]:
+            some_str = gen_str(some_len)
+            self.assertEqual(len(some_str), some_len)
