1901b215bb6cd733950d8f85bd0ea17ee6235c44
pdimitriu <pdimitri@spawar.navy.mil>
2023-04-03 14:54:10 -0700

    deleting a bunch of dead code

--- scuba/accounts/apis/buddies.py
+++ scuba/accounts/apis/buddies.py
@@ -119,9 +119,6 @@ class AcceptBuddyRequestApi(generics.CreateAPIView):
     """
     serializer_class = AcceptBuddyRequestSerializer
     def create(self, request, *args, **kwargs):
-
-        from pprint import pprint
-        pprint(kwargs)
         serializer = self.get_serializer(data=kwargs)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
@@ -132,7 +129,6 @@ class AcceptBuddyRequestApi(generics.CreateAPIView):
         }, status=status.HTTP_201_CREATED)
 
 
-
 @login_required
 @require_http_methods(["POST"])
 def invited(us_request):
@@ -189,62 +185,6 @@ def invited(us_request):
         response['resp'] = 'ok'
         return JSONResponse( response )
 
-
-@login_required
-@require_http_methods(["POST"])
-def invited(us_request):
-
-    user = us_request.user
-    email_invites = []
-    response = {'invalid': [], 'sent': [], 'resent': [], 'friends': []}
-
-    if us_request.is_ajax():
-        email_data = json.loads(us_request.body)
-
-        friend = None
-        from django.core.validators import validate_email
-
-        for email in email_data['email'].split(','):
-            # let's get the email and check if it's already in the system
-            try:
-                # make sure the email address is correct
-                validate_email(email)
-            except:
-                response['invalid'].append(email)
-                continue
-
-            # let's see if the user is being a jackass and is inviting himself
-            if user.email == email:
-                response['invalid'].append(email)
-                continue
-
-            friend = None
-            try:
-                friend = User.objects.get(email=email)
-            except:
-                pass
-
-            # ok, so far a valid email address.  now, let's check for a valid user
-            # first, is this this a valid usre with
-
-            if Friendship.objects.filter(
-                Q(friend1__email=email) |
-                Q(friend2__email=email)):
-                # the user is already a friend.  forget it
-                response['friends'].append(email)
-                continue
-
-            if UserFriendRequest.objects.filter(friend=user, email=email):
-                # the user has already been requested
-                response['resent'].append(email)
-                continue
-
-            # if we got down here, we can add the new user
-            UserFriendRequest.objects.create(friend=user, email=email, user=friend, active=True)
-            response['sent'].append(email)
-
-        response['resp'] = 'ok'
-        return JSONResponse( response )
 
 @login_required
 @require_http_methods(["PUT"])
@@ -254,73 +194,11 @@ def accept_invite(us_request):
     user = us_request.user
     return JSONResponse( response )
 
+
 @login_required
 @require_http_methods(["DELETE"])
 def delete_friendship(us_request):
     response = {}
 
     user = us_request.user
-    return JSONResponse( response )
-
-
-@login_required
-@require_http_methods(["PUT"])
-def add_friend(us_request):
-    response = {}
-    user = us_request.user
-
-    if us_request.is_ajax():
-        request_data = json.loads(us_request.body)
-        fid = request_data['fid']
-        friend = None
-
-        try:
-            friend = User.objects.get(account__guid=fid)
-        except:
-            pass
-
-
-        # now, let's try to create a user friend request for this particular user
-        try:
-            # this may seem backwards, but it is correct.  We want the the user object
-            # (at the end) to collect all of his friendships
-            UserFriendRequest.objects.create(user=friend, friend=user)
-        except IntegrityError:
-            response['error'] = True
-
-    return JSONResponse( response )
-
-
-@login_required
-@require_http_methods(["PUT"])
-def accept_request(us_request):
-    response = {}
-    user = us_request.user
-
-    if us_request.is_ajax():
-        request_data = json.loads(us_request.body)
-        fid = request_data['fid']
-        mode = request_data['mode']
-
-        friend = None
-        try:
-            friend = User.objects.get(account__guid=fid)
-        except:
-            pass
-
-        # now, let's try to get the friend request object
-        if mode == 'unblock':
-            UserFriendBlocked.objects.filter(user=user, friend=friend).delete()
-        else:
-            try:
-                request_obj = UserFriendRequest.objects.get(user=user, friend=friend)
-                if mode == 'add':
-                    Friendship.objects.create(friend1=user, friend2=friend).save()
-
-                # delete the request object
-                request_obj.delete()
-
-            except IntegrityError:
-                response['error'] = True
-
     return JSONResponse( response )
