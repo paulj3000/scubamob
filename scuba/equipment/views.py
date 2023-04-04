813ae6596b1e9ac462627aa250d9bf263a9050fa
pdimitriu <pdimitri@spawar.navy.mil>
2023-04-04 12:47:48 -0700

    more pep8 cleanup

--- scuba/equipment/views.py
+++ scuba/equipment/views.py
@@ -10,29 +10,31 @@ from scuba.equipment.models import EquipmentMaintenance, Equipment
 from scuba.equipment.forms import EquipmentForm
 from scuba.equipment.forms import EquipmentMaintenanceForm
 
+
 def index(us_request):
     equipment = Equipment.objects.all()
     t = loader.get_template("equipment/index.html")
-    context = Context({ 'equipment': equipment })
+    context = Context({'equipment': equipment})
     return HttpResponse(t.render(context))
+
 
 def edit(request, equipment_id=None):
     context = {}
-    if request.method == 'POST': # If the form has been submitted...
+    if request.method == 'POST':
         if equipment_id:
             equipment_obj = get_object_or_404(Equipment, pk=equipment_id)
 
-            ## let's verify the equipment matches the user
+            # let's verify the equipment matches the user
             if equipment_obj.user != request.user:
                 raise Http404
 
-            form = EquipmentForm(instance=equipment_obj, data=request.POST) # A form bound to the POST data
+            form = EquipmentForm(instance=equipment_obj, data=request.POST)
         else:
-            form = EquipmentForm(request.POST) # A form bound to the POST data
+            form = EquipmentForm(request.POST)
 
-        form.user    = request.user
+        form.user = request.user
 
-        if form.is_valid(): # All validation rules pass
+        if form.is_valid():
             new_addelement = form.save()
             return HttpResponseRedirect('/equipment/')
 
@@ -45,7 +47,7 @@ def edit(request, equipment_id=None):
     else:
         form = EquipmentForm()
 
-    return render(request, 'equipment/edit.html',  { 'form': form, 'equipment_id': equipment_id })
+    return render(request, 'equipment/edit.html',  {'form': form, 'equipment_id': equipment_id})
 
 
 def delete_equipment(request, equipment_id):
@@ -57,50 +59,52 @@ def delete_equipment(request, equipment_id):
     equipment_obj.delete()
     return HttpResponseRedirect('/equipment/')
 
+
 def archive2(request):
     posts = Equipment.objects.all()
     t = loader.get_template("archive2.html")
-    c = Context({ 'posts': posts })
+    c = Context({'posts': posts})
     return HttpResponse(t.render(c))
+
 
 def archive3(request):
     posts = EquipmentMaintenance.objects.all()
     t = loader.get_template("archive3.html")
-    c = Context({ 'posts': posts })
+    c = Context({'posts': posts})
     return HttpResponse(t.render(c))
 
+
 def practice_requirements(request):
-    if request.method == 'POST': # If the form has been submitted...
-        form = EquipmentMaintenanceForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
+    if request.method == 'POST':
+        form = EquipmentMaintenanceForm(request.POST)
+        if form.is_valid():
             new_requirements = form.save()
             return HttpResponseRedirect('/thanks/')
     else:
         form = EquipmentMaintenanceForm()
 
-    return render(request, 'add_requirement.html', { 'form': form})
+    return render(request, 'add_requirement.html', {'form': form})
+
 
 def practice_require_edit(request, requirements_id):
     requirements1 = get_object_or_404(EquipmentMaintenance, pk=requirements_id)
-    if request.method == 'POST': # If the form has been submitted...
+    if request.method == 'POST':
         form = EquipmentMaintenanceForm(instance=requirements1, data=request.POST)
-        if form.is_valid(): # All validation rules pass
-            requirements1= form.save()
+        if form.is_valid():     # All validation rules pass
+            requirements1 = form.save()
             return HttpResponseRedirect('/thanks/')
     else:
         form = EquipmentMaintenanceForm(instance=requirements1)
-    return render(request, 'add_requirement.html', { 'form': form})
+    return render(request, 'add_requirement.html', {'form': form})
+
 
 def delete_requirement(request, requirements_id):
     deleterequire2 = get_object_or_404(EquipmentMaintenance, pk=requirements_id)
     if request.method == 'POST':
         form = EquipmentMaintenanceForm(instance=deleterequire2, data=request.POST)
-        if form.is_valid(): # All validation rules pass
+        if form.is_valid():
             deleterequire2.delete()
             return HttpResponseRedirect('/thanks/')
     else:
         form = EquipmentMaintenanceForm(instance=deleterequire2)
-    return render(request, 'add_requirement.html', { 'form': form})
-
-
-
+    return render(request, 'add_requirement.html', {'form': form})
