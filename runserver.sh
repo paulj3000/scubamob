b7e78a096b7ff1ad14c2a6eb6bbe6bb993c02821
Pauljames Dimitriu <paulj1999@yahoo.com>
2023-04-05 16:22:39 -0700

    removing mongo from start server

--- runserver.sh
+++ runserver.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#if [[ -z $(docker ps | grep "mongo" | awk '{ print $1 }')  ]]; then
-#docker run -it -d -p 27017:27017 mongo
-#fi
-
 if [[ -z $(docker ps | grep "memcached" | awk '{ print $1 }')  ]]; then
 docker run -it -d -p 11211:11211 memcached
 fi
