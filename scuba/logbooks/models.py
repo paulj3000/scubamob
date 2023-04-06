e988c95c47a9b851d54bf45a8a4c0a9aab7f6a0e
Pauljames Dimitriu <paulj1999@yahoo.com>
2023-04-05 18:43:37 -0700

    removing GUID field from the logbooks

--- scuba/logbooks/models.py
+++ scuba/logbooks/models.py
@@ -26,9 +26,6 @@ class LogbookFolder(UUIDModel):
     user = models.ForeignKey('accounts.User', related_name='logbook_folders', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
 
-    def init_guid(self):
-        return str(uuid.uuid1()).replace('-', '')
-
     def get_logs(self):
         divelog_mongo = DiveLog()
         return divelog_mongo.get_log(user_id=self.user.id, folder=self.guid)
@@ -40,18 +37,7 @@ class LogbookFolder(UUIDModel):
 
 class LogbookTag(UUIDModel):
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
-    guid = models.CharField(max_length=40)
     name = models.CharField(max_length=255)
-
-    def init_guid(self):
-        return str(uuid.uuid1()).replace('-', '')
-
-    def save(self, *args, **kwargs):
-        # make sure we have a valid guid
-        self.guid = self.init_guid() if not self.guid else self.guid
-
-        # save the object
-        super().save(*args, **kwargs)
 
     class Meta:
         db_table = 'logbook_tag'
