38ea062de1ba096a9ee0c1b2e2800773ee1f633a
pdimitriu <pdimitri@spawar.navy.mil>
2023-04-11 09:38:36 -0700

    calling weather api straight

--- scuba/home/apis.py
+++ scuba/home/apis.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from django.core.cache import cache
@@ -7,11 +7,13 @@ from django.core.cache import cache
 from django.db.models import Q
 from scuba.home.models import Jumbotron
 from scuba.home.serializers import JumbotronSerializer
+from scuba.weather.serializers import WeatherSerializer
 from scuba.accounts.serializers.buddies import BuddySerializer, BuddyRecentActivity
 from scuba.accounts.models import User
 from scuba.divesites.models import Divesite
 from scuba.divesites.serializers import DivesiteSerializer
-from scuba.weather.models import Weather
+from scuba.sitesettings.models import APIKey
+from scuba.weather.libs.weather import Weather as WeatherAPI
 
 
 class GetJumbotronApi(generics.GenericAPIView):
@@ -51,22 +53,13 @@ class SearchApi(generics.GenericAPIView):
         return Response({'search': retval})
 
 
-class GetWeatherFromZip(generics.GenericAPIView):
-    serializer_class = JumbotronSerializer
-
-    def get(self, request):
-        return Response({
-            'jumbotron': 'x'
-        })
-
-
 class GetHomescreenApi(generics.GenericAPIView):
     def get(self, request):
         user = request.user
         buddy_recent_activity = user.get_all_buddies_recent_activity()
 
-        data = request.query_params
-        dist = data.get('distance', 100)
+        q_param = request.query_params.get('q', 92107)
+        '''
         if data.get('q'):
             if ',' in data['q']:
                 lat, lng = data['q'].split(',')
@@ -76,6 +69,7 @@ class GetHomescreenApi(generics.GenericAPIView):
                 weather = Weather.get_current_by_postal_code(code)
         else:
             weather = Weather.get_current_by_postal_code('92107')
+        '''
 
         return Response({
             'buddies': {
@@ -83,7 +77,8 @@ class GetHomescreenApi(generics.GenericAPIView):
                 'list': BuddySerializer(user.get_all_buddies(), many=True).data,
                 'recent_activity': BuddyRecentActivity(buddy_recent_activity, many=True).data,
             },
-            'weather': weather.data,
+            #'weather': WeatherSerializer(weather, many=True).data,
+            'weather': WeatherAPI.get_current_by_q_param(q_param),
             'divesites': {
                 'favorites': user.get_divesite_favorites(),
                 'list': DivesiteSerializer(Divesite.get_all_active_divesites(), many=True).data
