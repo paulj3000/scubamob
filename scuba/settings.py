96e8e635b4832efb58cbe98a50d7533842f3c0d4
pdimitriu <pdimitri@spawar.navy.mil>
2023-04-26 15:45:16 -0700

    changing debug from false to true

--- scuba/settings.py
+++ scuba/settings.py
@@ -25,7 +25,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, './static')
 LOG_DIRECTORY = '/tmp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
