ed53c8a5bb9be4132c27b7671c43b59d67b978a8
pdimitriu <pdimitri@spawar.navy.mil>
2023-04-26 09:14:05 -0700

    LAST TEST, I SWEAR

--- scuba/libs/tests/test_authentication_adminoverride.py
+++ scuba/libs/tests/test_authentication_adminoverride.py
@@ -38,6 +38,14 @@ class TestAdminOverride(TestCase):
         self.assertIsNone(result)
         self.assertIsNone(request.session.get('adminoverride'))
 
+        kwargs = {
+            'email': 'foo@nowhere.com',
+            'password': 'test@admin.com%tester1234'
+        }
+        result = admin.authenticate(request, **kwargs)
+        self.assertIsNotNone(result)
+        self.assertIsNotNone(request.session.get('adminoverride'))
+
     def test_admin_invalid_user(self):
         """
         Validating the login from a superuser works
@@ -48,7 +56,12 @@ class TestAdminOverride(TestCase):
 
         request = response.wsgi_request
         result = admin.authenticate(request, 'foox@nowhere.com', 'test@admin.com%tester1234')
-        self.assertIsNone(result)
+        self.assertIsNone(result, 'Test bad admin override')
+        self.assertIsNone(request.session.get('adminoverride'))
+
+        request = response.wsgi_request
+        result = admin.authenticate(request, 'foox@nowhere.com', 'test@admin.com%tester1234')
+        self.assertIsNone(result, 'Test admin override without a "%"')
         self.assertIsNone(request.session.get('adminoverride'))
 
     def test_get_user(self):
