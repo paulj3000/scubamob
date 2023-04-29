939f71c96d69c4dde967275ee30a97314efc29bd
Pauljames Dimitriu <paulj1999@yahoo.com>
2023-04-29 13:24:04 -0700

    adding first_name and last_name to search buddy serializer

--- scuba/home/serializers.py
+++ scuba/home/serializers.py
@@ -25,6 +25,8 @@ class JumbotronSerializer(serializers.ModelSerializer):
 class BuddySerializer(serializers.Serializer):
     id = serializers.SerializerMethodField()
     full_name = serializers.SerializerMethodField()
+    first_name = serializers.SerializerMethodField()
+    last_name = serializers.SerializerMethodField()
     profile_image = serializers.SerializerMethodField()
 
     @staticmethod
@@ -38,3 +40,11 @@ class BuddySerializer(serializers.Serializer):
     @staticmethod
     def get_id(data):
         return data.pk_as_str
+
+    @staticmethod
+    def get_first_name(data):
+        return data.first_name
+
+    @staticmethod
+    def get_last_name(data):
+        return data.last_name
