9df6c1335efdd4d26b18411582a97c08f921e879
Pauljames Dimitriu <paulj1999@yahoo.com>
2023-04-29 17:15:29 -0700

    fixing user profile crap

--- scuba/accounts/apis/profile.py
+++ scuba/accounts/apis/profile.py
@@ -1,43 +1,33 @@
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_http_methods
+from django.http import Http404
 from django.db.models import Q
-from django.db import IntegrityError
-from django.shortcuts import get_object_or_404
-from django.core.exceptions import ValidationError
-
-import json
 
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
 
 from scuba.accounts.models import User
-from scuba.accounts.exceptions import InvalidUserIdException
 from scuba.accounts.models import UserBlocked
-from scuba.accounts.serializers.profile import ProfileSerializer, \
-    BlockUserSerializer, AddBuddySerializer, \
-    CancelBuddyRequestSerializer, ConfirmBuddyRequestSerializer
-
-from scuba.accounts.serializers.buddies import BuddySerializer
+from scuba.accounts.serializers.profile import ProfileSerializer
 
 
 class GetProfileApi(generics.RetrieveAPIView):
-    lookup_field = 'email'
-    permission_classes = (IsAuthenticated,)
     serializer_class = ProfileSerializer
+    lookup_field = 'id'
 
     def get_queryset(self):
         id = self.kwargs.get(self.lookup_field)
+        inner_qs = UserBlocked.objects.filter(buddy=self.request.user)
+        user = User.objects.filter(id=id) \
+            .filter(is_private=False) \
+            .exclude(blocked_buddy__in=inner_qs)
 
-        kwargs = {}
-        kwargs[self.lookup_field] = id
-        return User.objects.filter(**kwargs)
+        if self.request.user == user:
+            return user
+
+        return user
 
 
 class GetMeProfileApi(generics.GenericAPIView):
     lookup_field = 'username'
-    permission_classes = (IsAuthenticated,)
     serializer_class = ProfileSerializer
 
     def get(self, request):
