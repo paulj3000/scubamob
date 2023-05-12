6a94a62c37216320257181d7172503c0f00e6caa
Pauljames Dimitriu <paulj1999@yahoo.com>
2023-05-11 23:41:02 -0700

    removing signals from 'apps.py'

--- scuba/sitesettings/apps.py
+++ scuba/sitesettings/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class SiteSettingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scuba.sitesettings'
-
-    def ready(self):
-        import scuba.sitesettings.signals
