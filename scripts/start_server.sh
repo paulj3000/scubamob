63eafbe14575fd0bc6da06e438fa3d3cf17f096f
Paul Dimitriu <paulj1999@yahoo.com>
2023-06-23 22:34:10 -0700

    it's a one line at a time kind of thing

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /scuba/apps/env/bin/activate && pip install -r requirements.txt
+source /scuba/apps/env/bin/activate && cd /scuba/apps/scubamob && pip install -r requirements.txt
 source /scuba/apps/env/bin/activate && cd /scuba/apps/scubamob && python manage.py migrate
 service nginx start
