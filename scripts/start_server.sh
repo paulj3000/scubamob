74aa6cc610bdc454633e8c6b2bb185e80649b13f
Paul Dimitriu <paulj1999@yahoo.com>
2023-06-24 00:03:17 -0700

    last test

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#source /scuba/apps/env/bin/activate && cd /scuba/apps/scubamob && pip install -r requirements.txt
-#source /scuba/apps/env/bin/activate && cd /scuba/apps/scubamob && python manage.py migrate
+source /scuba/apps/env/bin/activate && cd /scuba/apps/scubamob && pip install -r requirements.txt
+source /scuba/apps/env/bin/activate && cd /scuba/apps/scubamob && python manage.py migrate
 service nginx restart
 service scuba restart
