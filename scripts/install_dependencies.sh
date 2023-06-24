f62fd8676c1a28047749f15c8f4678314d6224e9
Paul Dimitriu <paulj1999@yahoo.com>
2023-06-23 23:45:06 -0700

    adding a couple of last minute dependencies adding a heading page

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt update -y
 apt upgrade -y
-apt install -y nginx python3.10-venv sqlite3 libmariadb-dev python-dev
+apt install -y nginx python3.10-venv sqlite3 libmariadb-dev python-dev libpython3.10-dev
