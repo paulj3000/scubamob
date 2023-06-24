e14e20ec7e1cf0d83354dc57447a1b109f4beb13
Paul Dimitriu <paulj1999@yahoo.com>
2023-06-23 23:57:35 -0700

    turning off the script and DB migrations

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-source /scuba/apps/env/bin/activate && cd /scuba/apps/scubamob && pip install -r requirements.txt
-source /scuba/apps/env/bin/activate && cd /scuba/apps/scubamob && python manage.py migrate
-service nginx start
+#source /scuba/apps/env/bin/activate && cd /scuba/apps/scubamob && pip install -r requirements.txt
+#source /scuba/apps/env/bin/activate && cd /scuba/apps/scubamob && python manage.py migrate
+service nginx restart
+service scuba restart
