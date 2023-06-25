a3bd4c2e3ab3da94bad53d4ff2091eb2fec2f13c
Paul Dimitriu <paulj1999@yahoo.com>
2023-06-24 23:01:47 -0700

    changing file permissions and user for the scubamob directory

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source /scuba/apps/env/bin/activate && cd /scuba/apps/scubamob && pip install -r requirements.txt
 source /scuba/apps/env/bin/activate && cd /scuba/apps/scubamob && python manage.py migrate
+chmod -R 775 /scuba/apps/scubamob
+chown -R ubuntu:ubuntu /scuba/apps/scubamob
 service nginx restart
 service scuba restart
