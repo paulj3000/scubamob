81d875891af9e1245cc1dec20a8f543a5d6d7a2b
Paul Dimitriu <paulj1999@yahoo.com>
2023-07-28 11:37:51 -0700

    working on the gitignore

--- static/react/src/home/Components/Buddies.js
+++ static/react/src/home/Components/Buddies.js
@@ -8,13 +8,13 @@ function Buddies(props) {
     return (
         <div className="row">
 
-            {props.buddies.list.map(buddy => (
+            {props.buddies.list.slice(0, 10).map(buddy => (
                 <div className="col-md-4 mb-5" key={buddy.id}>
                     <div className="bg-white rounded shadow-sm py-5 px-4">
                         <img className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" src={buddy.profile_image} alt="Card image cap" width="100" />
                         <h5 className="mb-0">{buddy.full_name}</h5>
                     </div>
-                    <div className="card-footer"><a className="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                    <div className="card-footer"><a className="btn btn-primary btn-sm" href="#!">View Profile</a></div>
                 </div>
             ))}
         </div>
