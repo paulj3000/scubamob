10bca35e0ad4151b163628f3d76e196feec4c767
Paul Dimitriu <paulj1999@yahoo.com>
2023-08-02 10:29:18 -0700

    adding others

--- static/react/src/home/Components/Home.js
+++ static/react/src/home/Components/Home.js
@@ -5,6 +5,7 @@ import React from 'react';
 import Favorites from "./Favorites";
 import Buddies from "./Buddies";
 import Location from "./Location";
+import Others from "./Others";
 
 
 class Home extends React.Component {
@@ -67,32 +68,7 @@ class Home extends React.Component {
                 </div>
 
                 <div className="row">
-                    {this.state.buddies.list.map(buddy => (
-                        <div className="col-md-4 mb-5" key={buddy.id}>
-                            <div className="card h-100">
-                                <div className="card-body">
-                                    <h2 className="card-title">Card Two</h2>
-                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos eligendi labore.</p>
-                                </div>
-                            </div>
-                            <div className="card-footer"><a className="btn btn-primary btn-sm" href="#!">More Info</a></div>
-                        </div>
-                    ))}
-                </div>
-
-
-                <div className="col-8 mx-auto">
-                    <ul className="list-group">
-                        <li className="list-group-item">
-                            <h5>Account Management</h5>
-                        </li>
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                            Close Account
-                                <span>
-                                    <i className="bi bi-arrow-right"></i>
-                                </span>
-                        </li>
-                    </ul>
+                    <Others divesites={divesites} />;
                 </div>
             </div>
         }
