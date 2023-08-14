ada9851b4693cf82c1ff7ffec8d36f99c7b8eb3c
pdimitriu <pdimitri@spawar.navy.mil>
2023-08-14 12:25:05 -0700

    adding

--- scuba/equipment/urls.py
+++ scuba/equipment/urls.py
@@ -9,13 +9,24 @@ from scuba.equipment.models import Equipment
 urlpatterns = [
     # Examples:
     path(r'', views.index, name="equipment_home"),
-    re_path(r'edit/(?P<equipment_id>[0-9]+)/?$', views.edit, name='equipment_edit_id'),
-    path(r'edit/', views.edit, name="equipment_edit"),
-    re_path(r'delete/(?P<equipment_id>[0-9]+)/?$', views.delete_equipment, name='equipment_delete'),
+    re_path(r'edit/(?P<equipment_id>[0-9]+)/?$',
+            views.edit,
+            name='equipment_edit_id'),
+    path(r'edit/',
+         views.edit,
+         name="equipment_edit"),
+    re_path(r'delete/(?P<equipment_id>[0-9]+)/?$',
+            views.delete_equipment,
+            name='equipment_delete'),
 
     path('practiceapp2/', views.archive2),
     path('requirementsview/', views.archive3),
     path('practicerequire/', views.practice_requirements),
-    re_path(r'^(?P<requirements_id>\d+)/practicerequire_edit/$', views.practice_require_edit, name='practice_require_edit'),
-    re_path(r'^(?P<requirements_id>\d+)/requirements_delete/$', views.delete_requirement, name='delete_requirement'),
+    re_path(r'^(?P<requirements_id>\d+)/practicerequire_edit/$',
+            views.practice_require_edit,
+            name='practice_require_edit'),
+
+    re_path(r'^(?P<requirements_id>\d+)/requirements_delete/$',
+            views.delete_requirement,
+            name='delete_requirement'),
 ]
