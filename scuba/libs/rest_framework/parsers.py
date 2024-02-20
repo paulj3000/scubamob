3326dc9118905ae53c3dcc0accc73ad588d0568d
Paul Dimitriu <paulj1999@yahoo.com>
2024-02-20 02:32:01 -0800

    pep8 fixes

--- scuba/libs/rest_framework/parsers.py
+++ scuba/libs/rest_framework/parsers.py
@@ -1,4 +1,5 @@
 from rest_framework.parsers import JSONParser
 
+
 class AWSJSONParser(JSONParser):
     media_type = 'text/plain'
--- scuba/system/admin.py
+++ scuba/system/admin.py
@@ -6,4 +6,5 @@ import scuba.system.models as system_models
 class CodeBuildJobAdmin(admin.ModelAdmin):
     list_display = ('project', 'build_status', 'start_time', 'end_time',)
 
+
 admin.site.register(system_models.CodebuildJob, CodeBuildJobAdmin)
