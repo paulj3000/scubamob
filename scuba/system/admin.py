3a1a59d194d28fb387a57c6919c7715094ddc994
Paul Dimitriu <paulj1999@yahoo.com>
2024-02-20 21:09:35 -0800

    fixing pep8 issues and getting codebuild to work

--- scuba/system/admin.py
+++ scuba/system/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 import scuba.system.models as system_models
 
 
+class CodeBuildProjectAdmin(admin.ModelAdmin):
+    list_display = ('project', 'last_successful_build',)
+
+
 class CodeBuildJobAdmin(admin.ModelAdmin):
-    list_display = ('project', 'build_status', 'start_time', 'end_time',)
+    list_display = ('project', 'branch', 'build_status', 'start_time', 'end_time',)
 
 
+admin.site.register(system_models.CodebuildProject, CodeBuildProjectAdmin)
 admin.site.register(system_models.CodebuildJob, CodeBuildJobAdmin)
