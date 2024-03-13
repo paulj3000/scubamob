0b61127843d80c0a330f5c022a0e692077d12ffb
Paul Dimitriu <paulj1999@yahoo.com>
2024-03-12 19:55:51 -0700

    fixing install

--- scripts/install_db_updates.sh
+++ scripts/install_db_updates.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #source env/bin/activate &&
 #python manage.py migrate
-cd /scuba/apps/scubamob && . env/bin/activate && python manage.py migrate
+cd /scuba/apps/scubamob && . env/bin/activate && python3 manage.py migrate
