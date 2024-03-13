52da9a6c49fc5adcbba0eda36f40aeeb3d31a27c
Paul Dimitriu <paulj1999@yahoo.com>
2024-03-12 20:06:08 -0700

    fixing install

--- scripts/install_server.sh
+++ scripts/install_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update -y
 apt upgrade -y
-apt install -y nginx python3.10-venv sqlite3 libmariadb-dev libpython3.10-dev
+apt install -y nginx python3.10-venv sqlite3 libmariadb-dev libpython3.10-dev python3-pip
 mkdir -p /scuba/{apps,cache,conf,db,logs,system}
 chown -R ubuntu:ubuntu /scuba/*
