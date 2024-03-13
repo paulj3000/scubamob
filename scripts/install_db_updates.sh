88c8ea92aafd9eec17efde8dcc20efd8a8ca1f15
Paul Dimitriu <paulj1999@yahoo.com>
2024-03-12 22:37:36 -0700

    buildspec

--- scripts/install_db_updates.sh
+++ scripts/install_db_updates.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-cd /scuba/apps
-python3 -m venv env
-. env/bin/activate
+. /scuba/apps/env/bin/activate
 cd /scuba/apps/scubamob
-pip install -r requirements.txt
 python3 manage.py migrate
---
+++ scripts/install_venv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /scuba/apps
+python3 -m venv env
+. env/bin/activate
+cd /scuba/apps/scubamob
+pip install -r requirements.txt
