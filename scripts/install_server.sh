92f277140e2f001fa7fcc279d802af965e079318
Paul Dimitriu <paulj1999@yahoo.com>
2024-03-14 20:33:52 -0700

    adding scuba.service from s3

--- scripts/install_server.sh
+++ scripts/install_server.sh
@@ -2,6 +2,6 @@
 apt update -y
 apt upgrade -y
 apt install -y nginx python3.10-venv sqlite3 libmariadb-dev libpython3.10-dev python3-pip
+pip install awscli
 mkdir -p /scuba/{apps,cache,conf,db,logs,system}
-touch /scuba/xxx
 chown -R ubuntu:ubuntu /scuba/*
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ ! -f /etc/systemd/system/scuba.service ]; then
+    aws s3 cp s3://scubamob-deploy/scripts/${DEPLOYMENT_GROUP_NAME}/scuba.service /etc/systemd/system/scuba.service
+    systemcl enable scuba
+fi
+
 chown -R ubuntu:ubuntu /scuba/apps/scubamob
 service nginx restart
 service scuba restart
