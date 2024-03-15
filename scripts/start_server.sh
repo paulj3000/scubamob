dfb8bcca80ac6f03fba87272b1f06bef20e0ed25
Paul Dimitriu <paulj1999@yahoo.com>
2024-03-14 21:06:40 -0700

    fixing configuration files

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,7 +2,11 @@
 
 if [ ! -f /etc/systemd/system/scuba.service ]; then
     aws s3 cp s3://scubamob-deploy/scripts/${DEPLOYMENT_GROUP_NAME}/scuba.service /etc/systemd/system/scuba.service
-    systemcl enable scuba
+    systemctl enable scuba
+fi
+
+if [ ! -f /etc/nginx/sites-enabled/scubamob.conf ]; then
+    aws s3 cp s3://scubamob-deploy/scripts/${DEPLOYMENT_GROUP_NAME}/scubamob.conf /etc/nginx/sites-enabled/scubamob.conf
 fi
 
 chown -R ubuntu:ubuntu /scuba/apps/scubamob
