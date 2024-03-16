b3e365a743b4cd9bace9dce59183e9652ef08471
Paul Dimitriu <paulj1999@yahoo.com>
2024-03-16 10:58:47 -0700

    adding flake8 report

--- scuba/system/admin.py
+++ scuba/system/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 import scuba.system.models as system_models
+from scuba.settings import STATIC_URL
 
 
-class CodeBuildJobAdminInline(admin.StackedInline):
+class CodeBuildJobAdminInline(admin.TabularInline):
     """ CodeBuildJob
     """
+    fields = ('build_status', 'branch', 'start_time', 'end_time', 'coverage', 'flake8')
+    readonly_fields = ('reports', 'branch', 'build_status', 'start_time', 'end_time',)
+
     model = system_models.CodebuildJob
     extra = 0
+    max_num = 0
+
+    def coverage(self, obj):
+        return mark_safe(f'<a target="_blank" href="{STATIC_URL}{obj.id}/coverage/">Coverage</a>')
+
+    def flake8(self, obj):
+        return mark_safe(f'<a target="_blank" href="{STATIC_URL}{obj.id}/flake-report/">Flake8</a>')
 
 
 class CodeBuildProjectAdmin(admin.ModelAdmin):
