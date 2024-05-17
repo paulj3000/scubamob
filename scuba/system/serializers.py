3a47e55b6c5518d4707b6f32e6e3d9570a41d3ba
Paul Dimitriu <paulj1999@yahoo.com>
2024-05-17 12:46:30 -0700

    removing execution_time

--- scuba/system/serializers.py
+++ scuba/system/serializers.py
@@ -63,7 +63,6 @@ class CodeBuildJobSerializer(serializers.ModelSerializer):
 
 
 class CodePipelineStateSerializer(serializers.ModelSerializer):
-    execution_time = serializers.CharField()
     project_name = serializers.CharField()
     pipeline_arn = serializers.CharField()
     payload = serializers.CharField()
