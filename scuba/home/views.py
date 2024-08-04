121a3c5c3716206fef274e04730fc712273751d2
Paul Dimitriu <paulj1999@yahoo.com>
2024-08-04 15:17:21 -0700

    updating home page, putting the active divesites on the homepage

--- scuba/home/views.py
+++ scuba/home/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
-# define the user data for this account
-from scuba.home.models import Jumbotron
+from scuba.divesites.models import Divesite
 
 
 class IndexView(TemplateView):
@@ -18,9 +17,9 @@ class IndexView(TemplateView):
         add more parameters to the context data
         """
         context = super().get_context_data(**kwargs)
-        #context.update({
-        #    'jumbotron': Jumbotron.get_active_jumbotron(),
-        #})
+        context.update({
+            'divesites': Divesite.get_all_active_divesites(),
+        })
 
         return context
 
