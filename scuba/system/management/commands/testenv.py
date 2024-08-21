d45ac53c1c506c6965782507512a8ae8ead1ca4d
Paul Dimitriu <paulj1999@yahoo.com>
2024-08-21 16:35:26 -0700

    x

--- scuba/system/management/commands/testenv.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        ''' update all account guids '''
-        print("Is this building .... ", os.environ.get('IS_BUILDING', -999))
